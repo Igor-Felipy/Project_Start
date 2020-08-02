fbaf8ef07e0b5d93f6b5627866e7b019cac7b8f4
Igor-Felipy <igor.felipy123@gmail.com>
2020-08-01 23:15:05 -0400

    loops

---
+++ C/loops.c
@@ -0,0 +1,30 @@
+# include <stdio.h>
+
+void main() {
+
+    for(int x=0;x<10;x++){
+
+    }
+    for(int x=0;;x++){
+
+    }
+
+
+    int x1 = 0;
+    while(x1<10){
+        x1++;
+    }
+    while(true){
+
+    }
+
+
+    int x2 = 0;
+    do{
+        x2++
+    } while(x2<10);
+    do{
+
+    } while(true);
+
+}
---
+++ Java/loops.java
@@ -0,0 +1,29 @@
+public class HelloWorld {
+
+    public static void main(String []args) {
+        for (int x=0;x<10;x++){
+
+        }
+        for (int x=0;;x++){
+
+        }
+
+
+        int x1 = 0;
+        while(x1<10){
+            x1++;
+        }
+        while(true){
+
+        }
+
+        int x2 =0;
+        do{
+            x2++;
+        }while(x2<10);
+        do{
+
+        }while(true);
+
+    }
+}
